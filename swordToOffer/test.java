887b9dbe1046d9f8e6ad84ae0a3da7076069c06e
si <894438380@qq.com>
2020-09-16 10:57:21 +0800

    添加了第二个测试文件

--- swordToOffer/test.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author SXC
- * @date 2020/9/16 10:48
- */
-public class test {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
-}
--- swordToOffer/test2.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author SXC
- * @date 2020/9/16 10:49
- */
-public class test2 {
-    public static void main(String[] args) {
-        System.out.println(2);
-    }
-}
